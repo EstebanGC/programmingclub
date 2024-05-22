54426141c780fb4e37dbae683443eb1e5bef0140
Esteban Carvajal <45727470+EstebanGC@users.noreply.github.com>
2024-05-22 13:19:08 +0000

    whatever

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 const App = () => {
   return (
     <div className="container">
-      <h1 className="title">Whatever</h1>
+      <h1 className="title">Whatever Hola</h1>
       <BasicForm />
     </div>
   );
